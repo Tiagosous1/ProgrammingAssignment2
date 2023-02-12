4f4c8147f68fb3703deb9e31842e203423cc99f5
Tiago Sousa <111656082+Tiagosous1@users.noreply.github.com>
2023-02-12 00:12:08 +0000

    added the code for inverse function

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    t <- NULL
+    set <- function(y) {
+        x <<- y
+        t <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) t <<- solve
+    getsolve <- function() t
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    t <- x$getsolve()
+    if(!is.null(t)) {
+        message("getting inversed matrix")
+        return(t)
+    }
+    data <- x$get()
+    t <- solve(data, ...)
+    x$setsolve(t)
+    t ## Return a matrix that is the inverse of 'x'
 }
